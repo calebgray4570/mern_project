363bce36ef5d5b5597207b445fe9f32097260dee
Caleb Gray <caleb.gray@xenetixsoftware.com>
2021-02-04 16:25:36 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts'
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png';
+import useStyles from './styles';
+
+const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch])
+
+    return (
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60"></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container> 
+            </Grow>
+        </Container>
+    );
+}
+
+export default App;
+
+// youtube video: https://www.youtube.com/watch?v=ngc9gnGgUdA
+// left off at - 1:01:26
